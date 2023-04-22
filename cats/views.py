91ea4cd49af52afcc39e75501dcda1a23e2b5ff2
tachen <tatoschka1234@users.noreply.github.com>
2023-04-22 16:15:48 +0100

    viewset and router

--- cats/views.py
+++ cats/views.py
@@ -1,17 +1,9 @@
-from rest_framework import generics
+from rest_framework import viewsets
 
 from .models import Cat
 from .serializers import CatSerializer
 
 
-class CatList(generics.ListCreateAPIView):
-    # возвращает всю коллекцию объектов (например, всех котиков)
-    # или может создать новую запись в БД
-    queryset = Cat.objects.all()
-    serializer_class = CatSerializer
-
-
-class CatDetail(generics.RetrieveUpdateDestroyAPIView):
-    # возвращает обновляет или удаляет объекты модели по одному
+class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,8 +1,14 @@
+from rest_framework.routers import SimpleRouter
+
 from django.urls import include, path
 
-from cats.views import CatList, CatDetail
+from cats.views import CatViewSet
+
+
+router = SimpleRouter()
+router.register('cats', CatViewSet)
+
 
 urlpatterns = [
-    path('cats/', CatList.as_view()),
-    path('cats/<int:pk>/', CatDetail.as_view()),
+    path('', include(router.urls)),
 ]
