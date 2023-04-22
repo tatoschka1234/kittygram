8b3714bd6fb66ca359a16e98b7735f07b65eb029
tachen <tatoschka1234@users.noreply.github.com>
2023-04-22 16:00:41 +0100

    generics

--- cats/views.py
+++ cats/views.py
@@ -1,34 +1,17 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics
 
 from .models import Cat
 from .serializers import CatSerializer
 
 
-# @api_view(['GET', 'POST'])
-# def cat_list(request):
-#     if request.method == 'POST':
-#         serializer = CatSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#     cats = Cat.objects.all()
-#     serializer = CatSerializer(cats, many=True)
-#     return Response(serializer.data)
+class CatList(generics.ListCreateAPIView):
+    # возвращает всю коллекцию объектов (например, всех котиков)
+    # или может создать новую запись в БД
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
 
 
-class APICat(APIView):
-    def get(self, request):
-        cats = Cat.objects.all()
-        serializer = CatSerializer(cats, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = CatSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class CatDetail(generics.RetrieveUpdateDestroyAPIView):
+    # возвращает обновляет или удаляет объекты модели по одному
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,10 +1,8 @@
 from django.urls import include, path
 
-from cats.views import APICat
+from cats.views import CatList, CatDetail
 
 urlpatterns = [
-   #path('cats/', cat_list),
-   path('cats/', APICat.as_view()),
+    path('cats/', CatList.as_view()),
+    path('cats/<int:pk>/', CatDetail.as_view()),
 ]
-
-
