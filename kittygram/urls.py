49cc3ac83a916f68ec50d48be700eab9c4a8a795
tachen <tatoschka1234@users.noreply.github.com>
2023-04-22 15:24:21 +0100

    view class instead of view f

--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
 
-from cats.views import cat_list
+from cats.views import APICat
 
 urlpatterns = [
-   path('cats/', cat_list),
+   #path('cats/', cat_list),
+   path('cats/', APICat.as_view()),
 ]
 
 
